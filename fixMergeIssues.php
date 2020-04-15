420d12a4519b72fe802c6d35bec07e19aa14e987
Dominik Sander <dominik.sander@googlemail.com>
2020-04-15 15:56:46 +0200

    stuff

---
+++ fixMergeIssues.php
@@ -0,0 +1,50 @@
+<?php
+include('./lib/csvImporter.php');
+include('./lib/utils.php');
+include_once('./lib/ov.php');
+
+$file = fopen( './import/TOHL11/TOHL11pre.ros','r');
+$oldData = array();
+
+$newHexData = '';
+for($i=0;$i<5000;$i++) {
+	$data = str_split(bin2hex(fread($file,86)),2);
+	$name = trim(hex2str(join(array_slice($data, 0, 20))));
+
+	if(hexdec($data[28]) > 0 && hexdec($data[27]) > 0 ) {
+		$oldData[$name] = $data;
+	}
+}
+
+$file2 = fopen( './import/TOHL11broken/TOHL11pre.ros','r');
+$newHexData = '';
+for($i=0;$i<5000;$i++) {
+	$data = str_split(bin2hex(fread($file2,86)),2);
+	$name = trim(hex2str(join(array_slice($data, 0, 20))));
+
+	if(hexdec($data[28]) < 100 && hexdec($data[27]) > 100 ) {
+		// übernehme alte daten, außer alter, contract und salary
+		$alter = $data[29];
+		$vertrag = $data[50];
+		$salary1 = $data[46];
+		$salary2 = $data[47];
+		$salary3 = $data[48];
+
+		$data = $oldData[$name];
+
+		$data[29] = $alter;
+		$data[50] = $vertrag;
+		$data[46] = $salary1;
+		$data[47] = $salary2;
+		$data[48] = $salary3;
+		
+	}
+
+	$newHexData .= implode('',$data);
+}
+
+$file3 = fopen( './export/TOHL11/TOHL11pre.ros','w+');
+fwrite($file3, hex2bin($newHexData));
+fclose($file);
+fclose($file2);
+fclose($file3);
