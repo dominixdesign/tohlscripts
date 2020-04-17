c44f65f622dceefac76a34721a54f31aa10032a1
Sander <Dominik.Sander@duerrdental.com>
2020-04-17 15:39:03 +0200

    push

--- fixMergeIssues.php
+++ fixMergeIssues.php
@@ -3,7 +3,7 @@ include('./lib/csvImporter.php');
 include('./lib/utils.php');
 include_once('./lib/ov.php');
 
-$file = fopen( './import/TOHL11/TOHL11pre.ros','r');
+$file = fopen( './export/TOHL11/TOHL11pre.ros','r');
 $oldData = array();
 
 $newHexData = '';
@@ -16,7 +16,7 @@ for($i=0;$i<5000;$i++) {
 	}
 }
 
-$file2 = fopen( './import/TOHL11broken/TOHL11pre.ros','r');
+$file2 = fopen( './import/TOHL11preLatest/TOHL11pre.ros','r');
 $newHexData = '';
 for($i=0;$i<5000;$i++) {
 	$data = str_split(bin2hex(fread($file2,86)),2);
@@ -27,27 +27,16 @@ for($i=0;$i<5000;$i++) {
 	}
 	// alter check
 	if($data[29] != $oldData[$name][29] && $data[29] != '00' && $oldData[$name][29] != '') {
-		echo $name . ' (Alter)' .  PHP_EOL;
 		$data[29] = $oldData[$name][29];
 	}
 
 	// werte check
-	$temp = $data;
-	$c = 0;
 	for($i=32;$i<=44;$i++) {
 		if(hexdec($data[$i]) < hexdec($oldData[$name][$i])) {
 			$data[$i] = $oldData[$name][$i];
 			$ii = $i;
-			$c++;
 		}
 	}
-	if($c>0) {
-		$newHexData .= implode('',$data);
-		echo $name . ' (Werte)'. PHP_EOL;
-	} else {
-		$newHexData .= implode('',$temp);
-	}
-
 	// if(hexdec($data[28]) < 100 && hexdec($data[27]) > 100 ) {
 	// 	// übernehme alte daten, außer alter, contract und salary
 	// 	$alter = $data[29];
@@ -69,8 +58,8 @@ for($i=0;$i<5000;$i++) {
 	$newHexData .= implode('',$data);
 }
 
-// $file3 = fopen( './export/TOHL11/TOHL11pre.ros','w+');
-//fwrite($file3, hex2bin($newHexData));
-//fclose($file3);
+$file3 = fopen( './export/TOHL11pre/TOHL11pre.ros','w+');
+fwrite($file3, hex2bin($newHexData));
+fclose($file3);
 fclose($file);
 fclose($file2);
